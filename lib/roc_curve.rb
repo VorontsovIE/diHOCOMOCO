f8fb17c94224e82432ee070992e084887b0a5769
prijutme4ty <prijutme4ty@gmail.com>
2015-08-21 15:29:53 +0300

    integration using trapeze method

--- lib/roc_curve.rb
+++ lib/roc_curve.rb
@@ -8,7 +8,7 @@ end
 
 def calculate_auc(roc_points)
   roc_points.each_cons(2).map{|pt_1, pt_2|
-    pt_1.tpr * (pt_2.fpr - pt_1.fpr)
+    (pt_1.tpr + pt_2.tpr) * (pt_2.fpr - pt_1.fpr) / 2.0
   }.inject(0.0, &:+)
 end
 
