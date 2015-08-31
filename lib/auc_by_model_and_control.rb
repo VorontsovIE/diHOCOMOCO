0ddeefe94690e3e6f25cb49c998cc28d87965886
prijutme4ty <prijutme4ty@gmail.com>
2015-08-31 16:57:32 +0300

    fix (models without controls have no AUCs)

--- lib/auc_by_model_and_control.rb
+++ lib/auc_by_model_and_control.rb
@@ -2,6 +2,7 @@ def auc_by_control_and_model(folder)
   result = Hash.new{|h,k| h[k] = {} }
   Models.all_models.each{|model|
     model_auc_fn = File.join(folder, model.uniprot, "#{model.full_name}.txt")
+    next  unless File.exist?(model_auc_fn)
     File.readlines(model_auc_fn).each{|line|
       control_name, auc = line.chomp.split("\t")
       result[control_name][model.full_name] = auc.to_f
@@ -14,6 +15,7 @@ def auc_by_model_and_control(folder)
   result = Hash.new{|h,k| h[k] = {} }
   Models.all_models.each{|model|
     model_auc_fn = File.join(folder, model.uniprot, "#{model.full_name}.txt")
+    next  unless File.exist?(model_auc_fn)
     File.readlines(model_auc_fn).each{|line|
       control_name, auc = line.chomp.split("\t")
       result[model.full_name][control_name] = auc.to_f
