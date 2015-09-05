f9c308bd2e9536bb682343601dee898605b15c02
prijutme4ty <prijutme4ty@gmail.com>
2015-09-05 11:37:31 +0300

    Extract AUC class. Remove old AUCs

---
+++ lib/auc_infos.rb
@@ -0,0 +1,104 @@
+require 'median'
+require 'models'
+
+# AUCs represents AUC values for a single TF (several models, several datasets; AUC for each model over each dataset)
+class AUCs
+  attr_reader :auc_by_model_and_dataset
+
+  # {model => {dataset => auc}}
+  # model should be an instance of class Model (see #best_model_among_collections)
+  def initialize(auc_by_model_and_dataset)
+    @auc_by_model_and_dataset = auc_by_model_and_dataset
+  end
+
+  def auc_by_dataset_and_model
+    @auc_by_dataset_and_model ||= begin
+      result = {}
+      auc_by_model_and_dataset.each{|model, auc_by_dataset|
+        auc_by_dataset.each{|dataset, auc|
+          result[dataset] ||= {}
+          result[dataset][model] = auc
+        }
+      }
+      result
+    end
+  end
+
+  def to_s
+    "<#{auc_by_model_and_dataset}>"
+  end
+
+  def ==(other)
+    other.is_a?(self.class) && auc_by_model_and_dataset == other.auc_by_model_and_dataset
+  end
+
+  def empty?
+    models.empty? || datasets.empty?
+  end
+
+  def models
+    auc_by_model_and_dataset.keys.sort
+  end
+
+  def datasets
+    @datasets ||= auc_by_model_and_dataset.values.flat_map(&:keys).uniq.sort
+  end
+
+  def dataset_qualities
+    @dataset_qualities ||= datasets.map{|dataset|
+      [dataset, mean(auc_by_dataset_and_model[dataset].values)]
+    }.to_h
+  end
+
+  def weighted_model_aucs
+    @weighted_model_aucs ||= begin
+      quality_norm_factor = dataset_qualities.values.inject(0.0, &:+)
+      models.map{|model|
+        weighted_auc = datasets.map{|dataset|
+          auc_by_model_and_dataset[model][dataset] * dataset_qualities[dataset]
+        }.inject(0.0, &:+) / quality_norm_factor
+        [model, weighted_auc]
+      }.to_h
+    end
+  end
+
+  def best_model_among_collections(collections)
+     best_model, best_auc = weighted_model_aucs.select{|model, auc|
+      collections.include?(model.collection_short_name)
+    }.max_by{|model, auc| auc }
+    best_model
+  end
+
+  def without_bad_datasets(min_auc)
+    datasets_to_retain = datasets.select{|dataset|
+      dataset_qualities[dataset] >= min_auc
+    }
+    auc_by_model_and_dataset_retain = auc_by_model_and_dataset.map{|model, auc_by_dataset|
+      auc_by_dataset_retain = datasets_to_retain.map{|dataset|
+        [dataset, auc_by_dataset[dataset]]
+      }.to_h
+      [model, auc_by_dataset_retain]
+    }.to_h
+    self.class.new(auc_by_model_and_dataset_retain)
+  end
+
+  def without_bad_models(min_auc)
+    auc_by_model_and_dataset_retain = auc_by_model_and_dataset.select{|model, auc_by_dataset|
+      weighted_model_aucs[model] >= min_auc
+    }.to_h
+    self.class.new(auc_by_model_and_dataset_retain)
+  end
+
+  # Loads data for a single TF
+  def self.from_folder(glob)
+    auc_by_model_and_dataset = FileList[glob].map{|fn|
+      model = Model.new_by_name(fn.pathmap('%n'))
+      auc_by_dataset = File.readlines(fn).map{|line|
+        dataset, auc = line.chomp.split("\t")
+        [dataset, auc.to_f]
+      }.to_h
+      [model, auc_by_dataset]
+    }.to_h
+    self.new(auc_by_model_and_dataset)
+  end
+end
